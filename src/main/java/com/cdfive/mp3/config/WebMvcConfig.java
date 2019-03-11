530b2947d0594dfc15c98241def21636658465e4
cdfive <176402936@qq.com>
2019-03-11 10:01:38 +0800

    remove hard code and make mvc static resource mapping in springboot profile yml files

--- src/main/java/com/cdfive/mp3/config/WebMvcConfig.java
+++ src/main/java/com/cdfive/mp3/config/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package com.cdfive.mp3.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,15 +10,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * @author cdfive
  */
+@Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Value("${resources.static.pathPatterns}")
+    private String staticPathPatterns;
+
+    @Value("${resources.static.resourcelocations}")
+    private String staticResourcelocations;
+
+    @Value("${resources.mp3.pathPatterns}")
+    private String mp3PathPatterns;
+
+    @Value("${resources.mp3.resourcelocations}")
+    private String mp3Resourcelocations;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/2017/mp3/**").addResourceLocations("file:E:/mp3/");
-//        registry.addResourceHandler("/2017/mp3/**").addResourceLocations("/opt/mp3/");
-
+        addResourceMapping(registry, staticPathPatterns, staticResourcelocations);
+        addResourceMapping(registry, mp3PathPatterns, mp3Resourcelocations);
         super.addResourceHandlers(registry);
+    }
+
+    private void addResourceMapping(ResourceHandlerRegistry registry, String pathPatterns, String resourceLocations) {
+        if (StringUtils.isBlank(pathPatterns) || StringUtils.isBlank(resourceLocations)) {
+            return;
+        }
+
+        registry.addResourceHandler(pathPatterns).addResourceLocations(resourceLocations);
+        log.info("addResourceMapping: " + pathPatterns + "=>" + resourceLocations);
     }
 }
